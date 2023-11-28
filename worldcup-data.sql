962f2715c9c0f554105397e44203f8e94064aada
Kwang1402 <hongquang2k4@gmail.com>
2023-11-28 10:26:22 +0700

    Dump data for PlayerPositions and StaffPositions

---
+++ worldcup-data.sql
@@ -0,0 +1,20 @@
+USE worldcup;
+
+#
+# Dumping data for table 'PlayerPositions'
+#
+
+INSERT INTO PlayerPositions (Id, Position) VALUES (1, 'Goalkeeper');
+INSERT INTO PlayerPositions (Id, Position) VALUES (2, 'Defender');
+INSERT INTO PlayerPositions (Id, Position) VALUES (3, 'Midfielder');
+INSERT INTO PlayerPositions (Id, Position) VALUES (4, 'Attacker');
+# 4 records
+
+#
+# Dumping data for table 'StaffPostions'
+#
+
+INSERT INTO StaffPositions (Id, Position) VALUES (1, 'Head Coach');
+INSERT INTO StaffPositions (Id, Position) VALUES (2, 'Assistant Coach');
+INSERT INTO StaffPositions (Id, Position) VALUES (3, 'Physio');
+# 3 records
