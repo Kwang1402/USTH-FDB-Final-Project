3b43a9797a7b91e3cb5cb2fae846436c84c8a0f6
Kwang1402 <hongquang2k4@gmail.com>
2023-11-28 10:48:59 +0700

    Dump data for Stadiums

--- worldcup-data.sql
+++ worldcup-data.sql
@@ -31,3 +31,19 @@ INSERT INTO Rounds (Id, RoundName) VALUES (5, 'Third place playoff');
 INSERT INTO Rounds (Id, RoundName) VALUES (6, 'Final');
 # 6 records
 
+#
+# Dumping data for table 'Stadiums'
+#
+
+INSERT INTO Stadiums (Id, StadiumName, City, Capacity) VALUES (1, 'Al Bayt Stadium', 'Al Khor', 68895);
+INSERT INTO Stadiums (Id, StadiumName, City, Capacity) VALUES (2, 'Lusail Stadium', 'Lusail', 88966);
+INSERT INTO Stadiums (Id, StadiumName, City, Capacity) VALUES (3, 'Ahmad bin Ali Stadium', 'Al Rayyan', 45032);
+INSERT INTO Stadiums (Id, StadiumName, City, Capacity) VALUES (4, 'Education City Stadium', 'Al Rayyan', 44667);
+INSERT INTO Stadiums (Id, StadiumName, City, Capacity) VALUES (5, 'Khalifa International Stadium', 'Al Rayyan', 45857);
+INSERT INTO Stadiums (Id, StadiumName, City, Capacity) VALUES (6, 'Al Thumama Stadium', 'Doha', 44400);
+INSERT INTO Stadiums (Id, StadiumName, City, Capacity) VALUES (7, 'Stadium 974', 'Doha', 44089);
+INSERT INTO Stadiums (Id, StadiumName, City, Capacity) VALUES (8, 'Al Janoub Stadium', 'Al Wakrah', 44325);
+# 8 records
+
+
+
