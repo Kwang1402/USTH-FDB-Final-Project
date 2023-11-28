cb8b0ba8365113f7cc63e805d6f5ae291b319aa4
Kwang1402 <hongquang2k4@gmail.com>
2023-11-28 10:33:42 +0700

    Dump data for Rounds

--- worldcup-data.sql
+++ worldcup-data.sql
@@ -18,3 +18,16 @@ INSERT INTO StaffPositions (Id, Position) VALUES (1, 'Head Coach');
 INSERT INTO StaffPositions (Id, Position) VALUES (2, 'Assistant Coach');
 INSERT INTO StaffPositions (Id, Position) VALUES (3, 'Physio');
 # 3 records
+
+#
+# Dumping data for table 'Rounds'
+#
+
+INSERT INTO Rounds (Id, RoundName) VALUES (1, 'Group stage');
+INSERT INTO Rounds (Id, RoundName) VALUES (2, 'Round of 16');
+INSERT INTO Rounds (Id, RoundName) VALUES (3, 'Quarter-finals');
+INSERT INTO Rounds (Id, RoundName) VALUES (4, 'Semi-finals');
+INSERT INTO Rounds (Id, RoundName) VALUES (5, 'Third place playoff');
+INSERT INTO Rounds (Id, RoundName) VALUES (6, 'Final');
+# 6 records
+
